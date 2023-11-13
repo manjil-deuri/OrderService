97532e121431030d785c4dd793aa0ac75139a706
MANJIL DEURI <manjil.d@gmail.com>
2023-11-13 14:47:06 +0530

    Add PAYMENT-SERVICE Feign client

---
+++ src/main/java/com/java/novice/external/client/PaymentService.java
@@ -0,0 +1,14 @@
+package com.java.novice.external.client;
+
+import com.java.novice.external.request.PaymentRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "PAYMENT-SERVICE/payment")
+public interface PaymentService {
+    @PostMapping
+    public ResponseEntity<Long> processPayment(@RequestBody PaymentRequest paymentRequest);
+}
